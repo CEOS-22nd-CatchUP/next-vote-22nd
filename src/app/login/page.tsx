fb31fa8a48f37bf4e82ab75f4ad4166018ef9c1b
yam <1669jsh@naver.com>
2025-11-14 02:46:29 +0900

    feat: Login, Signup 페이지 반응형 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,21 +3,21 @@ import Link from 'next/link';
 export default function Login() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="flex w-[550px] flex-col items-center justify-center">
-        <div className="mb-9 w-[550px] border-b-2 pb-5">LOGIN</div>
-        <div>
-          <form>
+      <div className="flex w-[90dvw] flex-col items-center justify-center md:w-[550px]">
+        <div className="mb-9 w-full border-b-2 pb-5">LOGIN</div>
+        <div className="w-full">
+          <form className="w-full">
             <div className="mb-4">
-              <input className="border-foreground h-12 w-[550px] border-2 p-3" placeholder="아이디를 입력해 주세요" />
+              <input className="border-foreground h-12 w-full border-2 p-3" placeholder="아이디를 입력해 주세요" />
             </div>
             <div className="mb-[34px]">
               <input
                 type="password"
-                className="border-foreground h-12 w-[550px] border-2 p-3"
+                className="border-foreground h-12 w-full border-2 p-3"
                 placeholder="비밀번호를 입력해 주세요"
               />
             </div>
-            <button className="bg-foreground text-background h-16 w-[550px]">로그인하기</button>
+            <button className="bg-foreground text-background h-16 w-full">로그인하기</button>
           </form>
         </div>
         <div className="mt-5.5">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,42 +5,58 @@ import { useState } from 'react';
 export default function SignUp() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="flex w-[550px] flex-col items-center justify-center">
-        <div className="mb-9 w-[550px] border-b-2 pb-5">SIGNUP</div>
-        <div>
-          <form className="flex flex-col">
-            <div className="mb-7">
+      <div className="flex w-[90dvw] flex-col items-center justify-center md:w-[550px]">
+        <div className="mb-9 w-full border-b-2 pb-5">SIGNUP</div>
+        <div className="w-full">
+          <form className="flex w-full flex-col">
+            <div className="mb-7 w-full">
               <ToggleButton />
             </div>
-            <div className="mb-14 flex items-center justify-between">
-              <div className="font-semibold">팀</div>
-              <input className="border-foreground h-12 w-[227px] border-b-2 p-3" placeholder="팀을 입력해 주세요" />
-              <div className="font-semibold">이름</div>
-              <input className="border-foreground h-12 w-[227px] border-b-2 p-3" placeholder="이름을 입력해 주세요" />
-            </div>
-            <div className="mb-7 flex items-center justify-between">
-              <div className="font-semibold">아이디</div>
-              <input className="border-foreground h-12 w-[368px] border-b-2 p-3" placeholder="아이디를 입력해 주세요" />
-            </div>
-            <div className="mb-7 flex items-center justify-between">
-              <div className="font-semibold">이메일</div>
-              <input className="border-foreground h-12 w-[368px] border-b-2 p-3" placeholder="이메일을 입력해 주세요" />
-            </div>
-            <div className="mb-7 flex items-center justify-between">
-              <div className="font-semibold">비밀번호</div>
+            <div className="mb-14 flex w-full flex-col gap-3 md:flex-row md:items-center md:justify-between">
+              <div className="hidden font-semibold md:block">팀</div>
               <input
-                className="border-foreground h-12 w-[368px] border-b-2 p-3"
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[227px]"
+                placeholder="팀을 입력해 주세요"
+              />
+
+              <div className="hidden font-semibold md:block">이름</div>
+              <input
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[227px]"
+                placeholder="이름을 입력해 주세요"
+              />
+            </div>
+            <div className="mb-7 flex flex-col md:flex-row md:items-center md:justify-between">
+              <div className="mb-2 font-semibold md:mb-0">아이디</div>
+              <input
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[368px]"
+                placeholder="아이디를 입력해 주세요"
+              />
+            </div>
+
+            <div className="mb-7 flex flex-col md:flex-row md:items-center md:justify-between">
+              <div className="mb-2 font-semibold md:mb-0">이메일</div>
+              <input
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[368px]"
+                placeholder="이메일을 입력해 주세요"
+              />
+            </div>
+
+            <div className="mb-7 flex flex-col md:flex-row md:items-center md:justify-between">
+              <div className="mb-2 font-semibold md:mb-0">비밀번호</div>
+              <input
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[368px]"
                 placeholder="비밀번호를 입력해 주세요"
               />
             </div>
-            <div className="mb-11 flex items-center justify-between">
-              <div className="font-semibold">비밀번호 재확인</div>
+
+            <div className="mb-11 flex flex-col md:flex-row md:items-center md:justify-between">
+              <div className="mb-2 font-semibold md:mb-0">비밀번호 재확인</div>
               <input
-                className="border-foreground h-12 w-[368px] border-b-2 p-3"
+                className="border-foreground h-12 w-full border-b-2 p-3 md:w-[368px]"
                 placeholder="비밀번호를 다시 입력해 주세요"
               />
             </div>
-            <button className="bg-foreground text-background h-16 w-[550px]">회원가입하기</button>
+            <button className="bg-foreground text-background h-16 w-full">회원가입하기</button>
           </form>
         </div>
       </div>
@@ -51,7 +67,7 @@ export default function SignUp() {
 function ToggleButton() {
   const [selected, setSelected] = useState<'left' | 'right'>('left');
   return (
-    <div className="border-foreground flex w-[550px] overflow-hidden rounded-lg border-2">
+    <div className="border-foreground flex w-full overflow-hidden rounded-lg border-2">
       <button
         type="button"
         onClick={() => setSelected('left')}
