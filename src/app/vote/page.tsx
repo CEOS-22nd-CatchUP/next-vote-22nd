a1dfb80d66735bc108ec01d655e6c7aaa1e31abd
jy000n <jayoonchang@naver.com>
2025-11-15 15:50:30 +0900

    feat: 투표 메인페이지 UI

---
+++ src/app/vote/page.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+export default function VoteMainPage() {
+  const voteCategories = [
+    { name: 'FE 파트장', voteUrl: '/vote/front', resultUrl: '/vote/front/result' },
+    { name: 'BE 파트장', voteUrl: '/vote/back', resultUrl: '/vote/back/result' },
+    { name: '데모데이', voteUrl: '/vote/demo', resultUrl: '/vote/demo/result' },
+  ];
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex w-[80dvw] justify-between whitespace-nowrap">
+        {voteCategories.map((category) => (
+          <div>
+            <div className="mx-[1vw] -mb-[1.5px] flex justify-between">
+              <div className="h-[2vw] w-[3vw] rounded-t-full border-2"></div>
+              <div className="h-[2vw] w-[3vw] rounded-t-full border-2"></div>
+            </div>
+            <div
+              key={category.name}
+              className="border-foreground mb-2 w-full rounded-lg border-2 px-[5vw] py-[7vw] text-center text-lg font-medium"
+            >
+              {category.name}
+            </div>
+            <div className="flex w-full flex-col gap-1">
+              <Link
+                href={category.voteUrl}
+                className="border-foreground w-full cursor-pointer rounded-lg border-2 py-2 text-center"
+              >
+                투표하기
+              </Link>
+              <Link href={category.resultUrl} className="border-foreground w-full rounded-lg border-2 py-2 text-center">
+                결과보기
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/vote/result/[part]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Demo() {
-  return (
-    <div>
-      <main>데모데이 투표 페이지입니다</main>
-    </div>
-  );
-}
