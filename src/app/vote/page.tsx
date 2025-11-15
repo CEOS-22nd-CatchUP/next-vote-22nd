5c65f52a40bd13bfaa2cc3390e827428aaf446f0
jy000n <jayoonchang@naver.com>
2025-11-15 18:44:59 +0900

    style: 투표 메인 페이지 UI 그리드로 수정 #1

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -4,17 +4,16 @@ import { voteCategories } from '@/constants/partCategories';
 export default function VoteMainPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="flex w-[80dvw] justify-between whitespace-nowrap">
+      <div className="my-5 grid w-[80dvw] grid-cols-1 gap-6 whitespace-nowrap md:grid-cols-3">
         {voteCategories.map((category) => (
-          <div key={category.name}>
-            <div className="mx-[1vw] -mb-[1.5px] flex justify-between">
-              <div className="h-[2vw] w-[3vw] rounded-t-full border-2"></div>
-              <div className="h-[2vw] w-[3vw] rounded-t-full border-2"></div>
-            </div>
+          <div key={category.name} className="flex flex-col items-center">
+            {/* 카테고리 */}
             <div className="border-foreground mb-2 w-full rounded-lg border-2 px-[5vw] py-[7vw] text-center text-lg font-medium">
               {category.name}
             </div>
-            <div className="flex w-full flex-col gap-1">
+
+            {/* 버튼 */}
+            <div className="flex w-full flex-col gap-2">
               <Link
                 href={category.voteUrl}
                 className="border-foreground w-full cursor-pointer rounded-lg border-2 py-2 text-center"
