ad0267202f33bb9a9f88e836d5e962f35eaf4af5
jy000n <jayoonchang@naver.com>
2025-12-15 22:30:32 +0900

    feat: 투표 alert 모달 반응형 추가 #5

--- src/components/vote/Alert.tsx
+++ src/components/vote/Alert.tsx
@@ -8,16 +8,16 @@ interface AlertProps {
 const Alert = ({ onCancelVote, onClose }: AlertProps) => {
   return (
     <div className="bg-background/80 fixed inset-0 flex items-center justify-center">
-      <div className="bg-background border-foreground flex w-[60vw] flex-col justify-between rounded-lg border-2 p-8 font-medium shadow-2xl">
+      <div className="bg-background border-foreground flex w-[74vw] max-w-200 flex-col justify-between rounded-lg border-2 px-5 py-7 text-sm font-medium shadow-2xl md:px-8 md:py-10 md:text-lg lg:py-12">
         {/* 프롬프트 */}
-        <p className="mb-5 text-center text-lg leading-relaxed whitespace-pre-line">
+        <p className="mb-5 text-center leading-relaxed whitespace-pre-line lg:mb-7 lg:text-xl lg:leading-9">
           한 개의 항목에만 투표하실 수 있습니다.
           <br />
           다시 투표하시겠습니까?
         </p>
 
         {/* 버튼 */}
-        <div className="flex gap-4">
+        <div className="flex gap-4 lg:gap-6">
           <button onClick={onCancelVote} className="flex-1 cursor-pointer rounded-lg border-2 py-3">
             예
           </button>
