1a44c2e449110c68edead63ab84180ba8f8b9f11
jy000n <jayoonchang@naver.com>
2025-12-19 20:00:08 +0900

    feat: TopNavbar 컴포넌트 반응형 설정 #4

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link';
 
 const TopNavbar = () => {
   return (
-    <div className="flex w-full justify-center pt-[1vh]">
-      <div className="flex h-8 w-[90vw] items-center justify-between border-2 p-2">
-        <Link href="/">2025 CEOS AWARD</Link>
+    <div className="flex w-full justify-center pt-[1.5vh]">
+      <div className="flex h-8 w-[90vw] items-center justify-between gap-5 border-2 p-2">
+        <Link href="/">
+          <span className="sm:hidden">CEOS</span>
+          <span className="hidden sm:inline">2025 CEOS AWARD</span>
+        </Link>
         <div className="flex gap-5">
           <Link href="/vote" className="cursor-pointer">
             VOTING
